1aa958fa0ba65e1a18240f8666f8c449b58e2cdc
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2012-03-06 17:33:44 +0100

    core: Version injection + test

--- cipol-core/src/main/java/net/cipol/core/APICore.java
+++ cipol-core/src/main/java/net/cipol/core/APICore.java
@@ -1,20 +1,31 @@
 package net.cipol.core;
 
+import javax.annotation.PostConstruct;
+
 import net.cipol.api.APIService;
 import net.cipol.api.model.VersionInformation;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class APICore implements APIService {
+	
+	private final Logger logger = LoggerFactory.getLogger(APIService.class);
 
 	private final String versionNumber;
 	
 	@Autowired
 	public APICore(@Value("${app.version}") String versionNumber) {
 		this.versionNumber = versionNumber;
+	}
+	
+	@PostConstruct
+	public void log() {
+		logger.info("Version {}", versionNumber);
 	}
 
 	@Override
