8f12d819ab352d8248a46a95a7899047ddb3777b
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2012-03-13 14:17:22 +0100

    web: Refactoring of previous integration tests into Groovy

--- cipol-web/src/test/groovy/net/cipol/web/it/ITGAPI.groovy
+++ cipol-web/src/test/groovy/net/cipol/web/it/ITGAPI.groovy
@@ -1,17 +1,38 @@
 package net.cipol.web.it
 
 import static org.junit.Assert.assertEquals
+import static org.junit.Assert.fail
 
 import groovyx.net.http.HTTPBuilder;
 
+import org.junit.Before;
 import org.junit.Test;
 
 class ITGAPI {
 	
+	def version
+	
+	@Before
+	void before() {
+		def props = new Properties()
+		getClass().getResourceAsStream("/Project.properties").withStream {
+			stream -> props.load(stream)
+		}
+		version = props["project.version"]
+		println("Version = $version")
+	}
+	
 	@Test
 	void version() {
 		def http = new HTTPBuilder('http://localhost:8080/cipol/api/')
-		assertEquals("", "");
+		http.get ( path: 'version' ) { resp, json ->
+			println("Response status : $resp.status")
+			println("Response content: $json")
+			def actualVersion = json.versionNumber
+			if (actualVersion != version) {
+				fail("Expected version $version but was $actualVersion")
+			}
+		}
 	}	
 
 }
