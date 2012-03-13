a8ef612876c8f0924be8f08709c3ccaf496d98aa
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2012-03-13 16:23:14 +0100

    web: Testing the validate() with no rule

--- cipol-web/src/test/groovy/net/cipol/web/it/ITGAPI.groovy
+++ cipol-web/src/test/groovy/net/cipol/web/it/ITGAPI.groovy
@@ -2,6 +2,7 @@ package net.cipol.web.it
 
 import static org.junit.Assert.assertEquals
 import static org.junit.Assert.assertFalse
+import static org.junit.Assert.assertNull
 import static org.junit.Assert.assertTrue
 import static org.junit.Assert.fail
 
@@ -53,6 +54,21 @@ class ITGAPI {
 				assertFalse(json.valid);
 				assertEquals(1, json.messages.size());
 				assertTrue(json.messages[0].startsWith("[CORE-002]"));				
+			}
+		}
+	}	
+	
+	@Test
+	void validate_no_rule() {
+		http.request ( Method.POST, ContentType.JSON ) {
+			uri.path = 'validate/9853bf50-6d1d-11e1-b0c4-0800200c9a66'
+			body = [author: 'albert', message: 'My message']
+			requestContentType = ContentType.JSON
+			response.success = { resp, json ->
+				println("Response status : $resp.status")
+				println("Response content: $json")
+				assertTrue(json.valid);
+				assertEquals(0, json.messages.size());				
 			}
 		}
 	}	
