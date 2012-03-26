cd58ae03879dd8db7d4fa72f5c5bcf37b2d8602d
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2012-03-26 15:29:34 +0200

    jira: Groovy service

--- cipol-jira/src/main/java/net/cipol/jira/JIRAConfig.java
+++ cipol-jira/src/main/java/net/cipol/jira/JIRAConfig.java
@@ -3,6 +3,9 @@ package net.cipol.jira;
 public class JIRAConfig {
 
 	private String id;
+	private String url;
+	private String user;
+	private String password;
 
 	public JIRAConfig() {
 	}
@@ -13,6 +16,30 @@ public class JIRAConfig {
 
 	public void setId(String id) {
 		this.id = id;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public String getUser() {
+		return user;
+	}
+
+	public void setUser(String user) {
+		this.user = user;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
 	}
 
 }
