20567f633955a9f13d369d70a8fbcb43a9e061bc
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2012-03-26 20:59:36 +0200

    connectors: Connection to CIPOL OK

--- connectors/subversion/common/cipol_lib.py
+++ connectors/subversion/common/cipol_lib.py
@@ -1,7 +1,69 @@
 import os
+import sys
+
+import cipol_env
+
+import json
+import httplib
 
 # Execute a command and gets its output
 def cmd (cmd):
 	output = os.popen(cmd, 'rt').read()
 	return output.strip()
+	
+# Validation
+def validate (message, author, paths):
+	# Builds the commit information
+	# a CommitInformation
+	# has
+	# an author,
+	# a message,
+	# a collection of string as paths
+	# .
+	ci = {
+		"message": message,
+		"author": author,
+		"paths": paths
+		}
+	sys.stderr.write ("ci = %s\n" % ci)
+	# Sends the request
+	conn = httplib.HTTPConnection(cipol_env.CIPOL_HOST)
+	try:
+		# Headers
+		headers = {
+			"Content-type": "application/json",
+			"Accept": "application/json"
+			}
+		# Content
+		data = json.dumps(ci)
+		sys.stderr.write ("data = %s\n" % data)
+		# Request
+		conn.request("POST", "%s/api/validate/%s" % (cipol_env.CIPOL_PATH, cipol_env.CIPOL_POLICY), data, headers)
+		# Gets the response
+		resp = conn.getresponse()
+		if (resp.status != 200):
+			return "HTTP %s %s" % (resp.status, resp.reason)
+		else:
+			# Gets the returned data
+			responseData = resp.read()
+			sys.stderr.write ("responseData = %s\n" % responseData)
+			# Parsing
+			# a ValidationReport
+			# has
+			# a boolean as success,
+			# a nullable string as message,
+			# a collection of ValidationDetail as details
+			# .
+			report = json.loads(responseData)
+			sys.stderr.write ("report = %s\n" % report)
+			# Interprets the data
+			if (report['success']):
+				return None
+			else:
+				return report['message']
+	finally:
+		conn.close()
+	
+	# OK
+	return "NOK"
 	
--- connectors/subversion/common/cipol_pre_commit.py
+++ connectors/subversion/common/cipol_pre_commit.py
@@ -3,10 +3,10 @@ import os
 
 import cipol_env
 from cipol_lib import cmd
+from cipol_lib import validate
 from cipol_svn import SVNLOOK
 
-import json
-import httplib
+# FIXME Uses a debug() method
 
 # Gets the list of committed paths for a transaction, without regard
 # to the action
@@ -25,26 +25,30 @@ def getCommittedPaths (repos, txn):
 
 def main(repos, txn):
 
+	# Logging
 	sys.stderr.write ("host = %s\npath = %s\npolicy = %s\n" % (cipol_env.CIPOL_HOST, cipol_env.CIPOL_PATH, cipol_env.CIPOL_POLICY));
 	
+	# Message
 	message  = cmd("%s log %s -t %s" % (SVNLOOK, repos, txn))
 	sys.stderr.write ("message = %s\n" % message)
 	
+	# Author
 	author = cmd("%s author %s -t %s" % (SVNLOOK, repos, txn))
 	sys.stderr.write ("author = %s\n" % author)
 	
+	# Paths
 	paths = getCommittedPaths (repos, txn)
 	sys.stderr.write ("paths = %s\n" % paths)
-
-	sys.exit(1)
 	
-	conn = httplib.HTTPConnection(cipol_env.CIPOL_HOST)
-	conn.request("GET", "%s/api/version" % (cipol_env.CIPOL_PATH))
-	resp = conn.getresponse()
-	data = resp.read()
-	sys.stderr.write ("status = %s, reason = %s\ndata = %s\n" % (resp.status, resp.reason, data))
-	o = json.loads(data)
-	sys.stderr.write ("version = %s\n" % (o['versionNumber']))
+	# Gets the validation message
+	validation = validate (message, author, paths)
+
+	# Not empty message => NOK
+	if (validation != ""):
+		sys.stderr.write (validation);
+		sys.exit(1)
+	else:
+		sys.exit(0)
 
 if __name__ == '__main__':
 	if len(sys.argv) < 3:
