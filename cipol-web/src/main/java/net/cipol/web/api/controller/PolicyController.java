b80dd3d91de8b0849d9006ae0981b57113078f82
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2012-04-23 11:44:06 +0200

    [ui] Policy page skeleton

---
+++ cipol-web/src/main/java/net/cipol/web/api/controller/PolicyController.java
@@ -0,0 +1,33 @@
+package net.cipol.web.api.controller;
+
+import net.cipol.api.PolicyService;
+import net.cipol.model.Policy;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/ui/policy")
+public class PolicyController {
+	
+	private final PolicyService policyService;
+	
+	@Autowired
+	public PolicyController(PolicyService policyService) {
+		this.policyService = policyService;
+	}
+
+	@RequestMapping(value = "/load/{uid}", method = RequestMethod.GET)
+	public String load (@PathVariable String uid, Model model) {
+		// Loads the policy
+		Policy policy = policyService.loadPolicy(uid);
+		model.addAttribute("policy", policy);
+		// OK
+		return "policy";
+	}
+
+}
