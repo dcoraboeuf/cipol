efc8ebad704357feabe90665a246fdc1d5b7c7e0
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2012-04-28 19:24:32 +0200

    [web] Form with one field

--- cipol-web/src/main/webapp/scripts/forms.js
+++ cipol-web/src/main/webapp/scripts/forms.js
@@ -1,10 +1,26 @@
 var Forms = new function () {
 	
+	function field_invalidate (el) {
+		el.addClass('macro-form-error');
+	}
+	
+	function text_validate_required (name) {
+		var field = Ext.fly(name);
+		var value = field.getValue();
+		if (value != "") {
+			return true;
+		} else {
+			field_invalidate(field);
+			return false;
+		}
+	}
+	
 	function text_field_edit () {
 		
 	}
 	
 	return {
+		text_validate_required: text_validate_required,
 		text_field_edit: text_field_edit
 	};
 	
