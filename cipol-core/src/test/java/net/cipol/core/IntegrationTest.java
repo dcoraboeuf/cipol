080305f13c00c6837a6703f456a65139b1f888b8
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2012-05-02 17:41:25 +0200

    [db] Integration tests (bare policy)

--- cipol-core/src/test/java/net/cipol/core/IntegrationTest.java
+++ cipol-core/src/test/java/net/cipol/core/IntegrationTest.java
@@ -16,16 +16,9 @@ import net.cipol.model.ValidationDetail;
 import net.cipol.model.ValidationReport;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = { "classpath*:META-INF/spring/*.xml" })
-@ActiveProfiles(profiles = { "test" })
-public class IntegrationTest {
+public class IntegrationTest extends AbstractIntegrationTest {
 
 	@Autowired
 	private APIService api;
