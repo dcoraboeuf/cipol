e0f94b810f08158af7895c536373ef6095bb4dc7
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2012-05-04 13:25:28 +0200

    [db] More unit tests

--- cipol-core/src/test/java/net/cipol/core/ConfigCoreTest.java
+++ cipol-core/src/test/java/net/cipol/core/ConfigCoreTest.java
@@ -6,6 +6,7 @@ import static org.junit.Assert.assertNull;
 import net.cipol.api.ConfigService;
 import net.cipol.core.test.Config1;
 import net.cipol.core.test.Config2;
+import net.cipol.core.test.Config3;
 
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -41,6 +42,26 @@ public class ConfigCoreTest extends AbstractIntegrationTest {
 		assertNotNull(config);
 		assertEquals("My name", config.getName());
 		assertEquals("http://github.com", config.getUrl());
+	}
+	
+	@Test
+	public void loadSeveralFields_1() {
+		Config3 config = configService.loadConfig(Config3.class, "1");
+		assertNotNull(config);
+		assertEquals("My name", config.getName());
+		assertEquals("http://github.com", config.getUrl());
+		assertEquals(false, config.isDisabled());
+		assertEquals(10, config.getHits());
+	}
+	
+	@Test
+	public void loadSeveralFields_2() {
+		Config3 config = configService.loadConfig(Config3.class, "2");
+		assertNotNull(config);
+		assertEquals("My name", config.getName());
+		assertEquals("http://github.com", config.getUrl());
+		assertEquals(true, config.isDisabled());
+		assertEquals(100, config.getHits());
 	}
 
 }
---
+++ cipol-core/src/test/java/net/cipol/core/test/Config3.java
@@ -0,0 +1,42 @@
+package net.cipol.core.test;
+
+public class Config3 {
+
+	private String name;
+	private String url;
+	private boolean disabled;
+	private int hits;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public boolean isDisabled() {
+		return disabled;
+	}
+
+	public void setDisabled(boolean disabled) {
+		this.disabled = disabled;
+	}
+
+	public int getHits() {
+		return hits;
+	}
+
+	public void setHits(int hits) {
+		this.hits = hits;
+	}
+
+}
