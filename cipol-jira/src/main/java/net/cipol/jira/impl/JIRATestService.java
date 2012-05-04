01d670464ea54bc3fd5a8fee847fcc3981d4caaa
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2012-05-04 14:34:06 +0200

    [db][jira] Unit tests OK

--- cipol-jira/src/main/java/net/cipol/jira/impl/JIRATestService.java
+++ cipol-jira/src/main/java/net/cipol/jira/impl/JIRATestService.java
@@ -4,15 +4,10 @@ import net.cipol.CipolProfiles;
 import net.cipol.api.ConfigService;
 import net.cipol.jira.JIRAConfig;
 import net.cipol.jira.model.JIRAIssue;
-import net.cipol.jira.model.JIRAIssueCollection;
 
-import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
-
-import com.google.common.base.Predicate;
-import com.google.common.collect.Iterables;
 
 @Service
 @Profile({CipolProfiles.TEST, CipolProfiles.IT})
@@ -25,15 +20,7 @@ class JIRATestService extends JIRAServiceImpl {
 
 	@Override
 	public JIRAIssue getIssue(JIRAConfig jiraConfig, final String key) {
-		// Loads issues for this config
-		JIRAIssueCollection issues = configService.loadConfig(JIRAIssueCollection.class, jiraConfig.getId());
-		// OK
-		return Iterables.find(issues.getIssues(), new Predicate<JIRAIssue>(){
-			@Override
-			public boolean apply(JIRAIssue issue) {
-				return StringUtils.equals(key, issue.getKey());
-			}
-		}, null);
+		return configService.loadConfig(JIRAIssue.class, key);
 	}
 
 }
