c86331803ea736b61831316ffc3a57963676923d
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2012-05-08 12:57:54 +0200

    [sec] Log out link

--- cipol-web/src/main/java/net/cipol/web/ui/controller/TopController.java
+++ cipol-web/src/main/java/net/cipol/web/ui/controller/TopController.java
@@ -2,6 +2,8 @@ package net.cipol.web.ui.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.View;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 public class TopController {
@@ -9,6 +11,14 @@ public class TopController {
 	@RequestMapping("/login")
 	public String login() {
 		return "login";
+	}
+
+	/**
+	 * On logout, goes back to the home page to force re-login
+	 */
+	@RequestMapping("/logged-out")
+	public View loggedOut() {
+		return new RedirectView("/ui/", true, false, false);
 	}
 
 }
