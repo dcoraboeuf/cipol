5dca3c50bd88d801b2448e62939d671f39e7db5a
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2012-05-09 13:01:37 +0200

    [ui] Cancelling an edition

--- cipol-web/src/main/webapp/scripts/forms.js
+++ cipol-web/src/main/webapp/scripts/forms.js
@@ -17,24 +17,53 @@ var Forms = new function () {
 		}
 	}
 	
+	function text_field_cancel (id) {
+		var input = Ext.get('macro-form-input-' + id);
+		// Restores the old value
+		var oldValue = input.dom.getAttribute("oldvalue");
+		input.dom.value = oldValue;
+		// Disables the text field for edition
+		input.dom.setAttribute("readonly", "readonly");
+		// Enables the edit action button
+		Ext.fly('macro-form-action-edit-' + id).removeClass('action-hidden');
+		Ext.fly('macro-form-action-accept-' + id).addClass('action-hidden');
+		Ext.fly('macro-form-action-cancel-' + id).addClass('action-hidden');
+	}
+	
 	function text_field_edit (id) {
+		
 		// Enables the text field for edition
 		var input = Ext.get('macro-form-input-' + id);
 		input.dom.removeAttribute("readonly");
+		
 		// Registers event on ENTER
 		input.on('keypress', function (e) {
 			if (e.getCharCode() == 13) {
 				text_field_accept(id);
 			}
 		});
+		
 		// Requires the focus
 		input.focus();
 		input.dom.select();
-		// Disables the edit action button
+		
+		// Hides the edit action button
 		Ext.fly('macro-form-action-edit-' + id).addClass('action-hidden');
-		// Enables the validation / cancel buttons
-		Ext.fly('macro-form-action-accept-' + id).removeClass('action-hidden');
-		Ext.fly('macro-form-action-cancel-' + id).removeClass('action-hidden');
+
+		// Validation button
+		var acceptButton = Ext.get('macro-form-action-accept-' + id);
+		acceptButton.removeClass('action-hidden');
+		acceptButton.on('click', function () {
+			text_field_accept(id);
+		});
+		
+		// Cancel button
+		var cancelButton = Ext.get('macro-form-action-cancel-' + id);
+		cancelButton.removeClass('action-hidden');
+		cancelButton.on('click', function () {
+			text_field_cancel(id);
+		});
+		
 	}
 	
 	return {
