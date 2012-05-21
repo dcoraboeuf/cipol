a864621387462fcd76405badf2a06b7d12fffb26
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2012-05-21 17:39:59 +0200

    #7 Audit at the correct level

--- cipol-core/src/main/java/net/cipol/core/APICore.java
+++ cipol-core/src/main/java/net/cipol/core/APICore.java
@@ -5,7 +5,6 @@ import java.util.Locale;
 
 import javax.annotation.PostConstruct;
 
-import net.cipol.CipolLoggers;
 import net.cipol.api.APIService;
 import net.cipol.api.PolicyService;
 import net.cipol.api.RuleService;
@@ -24,7 +23,6 @@ import net.cipol.rule.RuleExecutionResultType;
 import net.sf.jstring.Strings;
 
 import org.apache.commons.lang.StringUtils;
-import org.apache.commons.lang3.ObjectUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -40,7 +38,6 @@ import com.google.common.collect.Iterables;
 public class APICore implements APIService {
 
 	private final Logger logger = LoggerFactory.getLogger(APIService.class);
-	private final Logger audit = LoggerFactory.getLogger(CipolLoggers.API_AUDIT);
 	
 	private final PathMatcher pathMatcher = new AntPathMatcher();
 
@@ -74,71 +71,53 @@ public class APICore implements APIService {
 	@Override
 	public ValidationReport validate(String policyId,
 			CommitInformation information) {
-		// Validation report to return
+		logger.debug("[validate] Request for policy [{}]", policyId);
+		// Loads the policy definition
+		final Policy policy = policyService.loadPolicy (policyId);
+		logger.debug("[validate] Applying policy [{}]", policy.getName());
+		// Execution context
+		final RuleExecutionContext context = new RuleExecutionContext() {
+			
+			@Override
+			public String getMessage(String key, Object... parameters) {
+				return strings.get(getLocale(), key, parameters);
+			}
+			
+			@Override
+			public boolean isMemberOfGroup(final String author, final String group) {
+				return isMemberOfPolicyGroup(policy, author, group);
+			}
+		};
+		// For each rule of the policy
 		ValidationReport validationReport = new ValidationReport();
-		try {
-			logger.debug("[validate] Request for policy [{}]", policyId);
-			// Loads the policy definition
-			final Policy policy = policyService.loadPolicy (policyId);
-			logger.debug("[validate] Applying policy [{}]", policy.getName());
-			// Execution context
-			final RuleExecutionContext context = new RuleExecutionContext() {
-				
-				@Override
-				public String getMessage(String key, Object... parameters) {
-					return strings.get(getLocale(), key, parameters);
-				}
-				
-				@Override
-				public boolean isMemberOfGroup(final String author, final String group) {
-					return isMemberOfPolicyGroup(policy, author, group);
-				}
-			};
-			// For each rule of the policy
-			List<RuleSet> rules = policy.getRules();
-			for (RuleSet ruleSet : rules) {
-				// Path pattern
-				String ruleSetPath = ruleSet.getPath();
-				if (StringUtils.isBlank(ruleSetPath)) {
-					ruleSetPath = DEFAULT_PATH;
-				}
-				// Disabled?
-				if (ruleSet.isDisabled()) {
-					logger.debug("[validate] Rule set for path [{}] is disabled", ruleSetPath);
-					continue;
-				}
-				logger.debug("[validate] Checking rule set for path [{}]", ruleSetPath);
-				// Is this rule set appliable?
-				if (isPathAppliable(ruleSetPath, information.getPaths())) {
-					// Applies this rule set
-					boolean abort = applyRuleSet (context, validationReport, ruleSet, information);
-					// Aborting?
-					if (abort) {
-						logger.debug("[validate] Aborting after applying rule set for path [{}]", ruleSetPath);
-						break;
-					}
+		List<RuleSet> rules = policy.getRules();
+		for (RuleSet ruleSet : rules) {
+			// Path pattern
+			String ruleSetPath = ruleSet.getPath();
+			if (StringUtils.isBlank(ruleSetPath)) {
+				ruleSetPath = DEFAULT_PATH;
+			}
+			// Disabled?
+			if (ruleSet.isDisabled()) {
+				logger.debug("[validate] Rule set for path [{}] is disabled", ruleSetPath);
+				continue;
+			}
+			logger.debug("[validate] Checking rule set for path [{}]", ruleSetPath);
+			// Is this rule set appliable?
+			if (isPathAppliable(ruleSetPath, information.getPaths())) {
+				// Applies this rule set
+				boolean abort = applyRuleSet (context, validationReport, ruleSet, information);
+				// Aborting?
+				if (abort) {
+					logger.debug("[validate] Aborting after applying rule set for path [{}]", ruleSetPath);
+					break;
 				}
 			}
-			// Consolidation
-			consolidate(validationReport);
-			// Result
-			return validationReport;
-		} finally {
-			// Audit message
-			audit (policyId, information, validationReport);
 		}
-	}
-
-	protected void audit(String policyId, CommitInformation information, ValidationReport validationReport) {
-		// Message
-		// TODO Id of the commit information (tx number for SVN for example)
-		String message = String.format("policy=%s,author=%s,success=%s,message=%s",
-				policyId,
-				information.getAuthor(),
-				validationReport.isSuccess(),
-				ObjectUtils.toString(validationReport.getMessage(), ""));
-		// Logging
-		audit.info(message);
+		// Consolidation
+		consolidate(validationReport);
+		// Result
+		return validationReport;
 	}
 
 	protected Locale getLocale() {
