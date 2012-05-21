b17a8c48677279ba70dac39a119c354eba613751
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2012-05-21 15:35:20 +0200

    #7 Using Spring profiles for log configuration

---
+++ cipol-web/src/main/java/net/cipol/web/support/DevConfig.java
@@ -0,0 +1,27 @@
+package net.cipol.web.support;
+
+import java.io.FileNotFoundException;
+
+import javax.annotation.PostConstruct;
+
+import net.cipol.CipolProfiles;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+import org.springframework.util.Log4jConfigurer;
+
+@Component
+@Profile({CipolProfiles.TEST, CipolProfiles.IT, CipolProfiles.DEV})
+public class DevConfig {
+	
+	private final Logger logger = LoggerFactory.getLogger("CONFIG");
+
+	@PostConstruct
+	public void init() throws FileNotFoundException {
+		Log4jConfigurer.initLogging("classpath:/log4j_dev.properties");
+		logger.info("Using DEV configuration");		
+	}
+
+}
---
+++ cipol-web/src/main/java/net/cipol/web/support/ProdConfig.java
@@ -0,0 +1,27 @@
+package net.cipol.web.support;
+
+import java.io.FileNotFoundException;
+
+import javax.annotation.PostConstruct;
+
+import net.cipol.CipolProfiles;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+import org.springframework.util.Log4jConfigurer;
+
+@Component
+@Profile(CipolProfiles.PROD)
+public class ProdConfig {
+	
+	private final Logger logger = LoggerFactory.getLogger("CONFIG");
+
+	@PostConstruct
+	public void init() throws FileNotFoundException {
+		Log4jConfigurer.initLogging("classpath:/log4j_prod.properties");
+		logger.info("Using PROD configuration");
+	}
+
+}
