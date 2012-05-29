bd2d7c3ff5b937ccb17da4e534591ad32d808b54
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2012-05-29 22:11:56 +0200

    [gwt] Starting with layout and CSS

--- cipol-gwt/src/main/java/net/cipol/gwt/home/client/Home.java
+++ cipol-gwt/src/main/java/net/cipol/gwt/home/client/Home.java
@@ -2,16 +2,32 @@ package net.cipol.gwt.home.client;
 
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.dom.client.Style.Unit;
+import com.google.gwt.user.client.ui.DockLayoutPanel;
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.RootPanel;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
  */
 public class Home implements EntryPoint {
 
-  /**
-   * This is the entry point method.
-   */
-  public void onModuleLoad() {
-	  GWT.log("Loading home page...");
-  }
+	/**
+	 * This is the entry point method.
+	 */
+	public void onModuleLoad() {
+		GWT.log("Loading home page...");
+		DockLayoutPanel p = new DockLayoutPanel(Unit.EM);
+		p.addNorth(createHeader(), 2);
+		p.addSouth(new HTML("footer"), 2);
+		//p.addWest(new HTML("west"), 2);
+		p.add(new HTML("My content"));
+		RootPanel.get().add(p);
+	}
+
+	private HTML createHeader() {
+		HTML html = new HTML("header");
+		html.addStyleName("header");
+		return html;
+	}
 }
