3366b1503c49a2c28fd9ea54cfbb7038826c0b31
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2012-05-29 21:50:36 +0200

    [gwt] Blank home page

---
+++ cipol-gwt/src/main/java/net/cipol/gwt/home/client/Home.java
@@ -0,0 +1,15 @@
+package net.cipol.gwt.home.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+/**
+ * Entry point classes define <code>onModuleLoad()</code>.
+ */
+public class Home implements EntryPoint {
+
+  /**
+   * This is the entry point method.
+   */
+  public void onModuleLoad() {
+  }
+}
