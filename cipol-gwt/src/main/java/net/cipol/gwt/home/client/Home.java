b702dcf6f9a7d5cd40d5fe5de871c3bdce698567
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2012-05-29 21:53:04 +0200

    [gwt] Logging...

--- cipol-gwt/src/main/java/net/cipol/gwt/home/client/Home.java
+++ cipol-gwt/src/main/java/net/cipol/gwt/home/client/Home.java
@@ -1,6 +1,7 @@
 package net.cipol.gwt.home.client;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.core.client.GWT;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
@@ -11,5 +12,6 @@ public class Home implements EntryPoint {
    * This is the entry point method.
    */
   public void onModuleLoad() {
+	  GWT.log("Loading home page...");
   }
 }
